0d31366e6b6ee5c1d00148dc62a27b1fac7fb4b1
Emily Zhang <emily49@stanford.edu>
2024-06-06 13:38:58 -0700

    change

--- website/src/App.js
+++ website/src/App.js
@@ -43,14 +43,14 @@ function App() {
           <h2>Abstract</h2>
           In this paper, we propose a method for 3D-consistent scene generation to create diverse,
           visually appealing, and infinite walkthroughs of specific films. Our approach builds on
-          the SceneScape pipeline and leverages Dreambooth to train Stable Diffusion models for
-          few-shot, scene-driven generation of novel views with consistent characters, settings, and
-          styles. We use ControlNet to condition these models for inpainting tasks, ensuring
-          structurally and contextually coherent scenes as the model inpaints the masks that are
-          recursively generated from the depth estimation model and the constructed 3D mesh. We
-          conducted experiments on four stylistically distinct films, and our method outperforms the
-          baseline on both quantitative aesthetic metrics and qualitative human evaluations, based
-          on content and style resemblance with the target film.
+          the SceneScape pipeline and uses Dreambooth to train Stable Diffusion models for few-shot,
+          scene-driven generation of novel views with specific characters, settings, and styles. We
+          use ControlNet to condition these models for inpainting tasks, ensuring structurally and
+          contextually coherent scenes as the model inpaints the masks recursively generated from
+          the depth estimation model and the constructed 3D mesh. We conducted experiments on four
+          stylistically distinct films, and our method outperforms the baseline on both quantitative
+          aesthetic metrics and qualitative human evaluations, based on content and style
+          resemblance with the target film.
         </div>
       </div>
       <Carousel arrows className={styles.carousel} afterChange={onChange}>
