0ca7368218c7b4086667342b195a98db539d77b4
Emily Zhang <emily49@stanford.edu>
2024-06-06 01:42:39 -0700

    abstract change

--- website/src/App.js
+++ website/src/App.js
@@ -45,11 +45,12 @@ function App() {
           appealing, and infinite walkthroughs of specific films. Our approach builds on the
           SceneScape pipeline and leverages Dreambooth to train Stable Diffusion models for
           few-shot, scene-driven generation of novel views with consistent characters, settings, and
-          styles. We also utilize ControlNet to effectively condition these models for inpainting
-          tasks, thereby ensuring both structurally and contextually coherent generated scenes. We
-          conducted experiments on four stylistically distinct films, and our method outperforms the
-          baseline on both quantitative aesthetic metrics and qualitative human evaluations, based
-          on content and style resemblance with the target film.
+          styles. We use ControlNet to condition these models for inpainting tasks, ensuring
+          structurally and contextually coherent scenes as the model inpaints the masks that are
+          generated from the depth estimation model and the constructed 3D mesh. We conducted
+          experiments on four stylistically distinct films, and our method outperforms the baseline
+          on both quantitative aesthetic metrics and qualitative human evaluations, based on content
+          and style resemblance with the target film.
         </div>
       </div>
       <Carousel arrows className={styles.carousel} afterChange={onChange}>
